b9a3764298b1c176045a8d479b438cd6e0b2fd1e
Martin Miglio <marmig0404@gmail.com>
2023-01-17 02:05:34 -0500

    Updates

---
+++ _next/static/4EU09XzUs6wrhtpnwmyZ7/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/188-a6e5cc043d66e0c2.js","static/css/08e439f3afb42ce6.css","static/chunks/pages/index-43b718bb9aaed7c2.js"],"/_error":["static/chunks/pages/_error-dfcfa5bb62767c20.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
